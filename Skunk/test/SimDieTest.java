6fe1cd9b235b3f498d2d7a92fb2c29f119f3812b
hoveronica <hoveronica01@gmail.com>
2018-02-16 11:32:26 -0600

    removed redundant test cases to have cleaner and more compact code

--- Skunk/test/SimDieTest.java
+++ Skunk/test/SimDieTest.java
@@ -7,78 +7,46 @@ public class SimDieTest
 	/*
 	 * * removed test_constructor test because it is being tested in all the following tests
 	 */
-	
+	/*
+	 * reduce redundant rolling tests
+	 * */
 	@Test
-	public void test_roll_once()
+	public void test_roll()
 	{
-		SimDie d = new SimDie(new int[] {1,2,3});
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-	}
-	
-
-	@Test
-	public void test_roll_twice()
-	{
-		SimDie d = new SimDie(new int[] {1,2,3});
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-		d.roll();
-		assertEquals(d.getLastRoll(),2);
-	}
-	
-	@Test
-	public void test_roll_three_times()
-	{
-		SimDie d = new SimDie(new int[] {1,2,3});
+		SimDie d = new SimDie(new int[] {1,2,3,4,5,6});
 		d.roll();
 		assertEquals(d.getLastRoll(),1);
 		d.roll();
 		assertEquals(d.getLastRoll(),2);
 		d.roll();
 		assertEquals(d.getLastRoll(),3);
+		d.roll();
+		assertEquals(d.getLastRoll(),4);
+		d.roll();
+		assertEquals(d.getLastRoll(),5);
+		d.roll();
+		assertEquals(d.getLastRoll(),6);
 	}
 	
 	@Test
-	public void test_roll_three_times_two_values()
+	public void test_roll_wrap_around()
 	{
-		SimDie d = new SimDie(new int[] {1,2});
+		SimDie d = new SimDie(new int[] {1,2,3,4,5,6});
+		d.roll();
+		d.roll();
+		d.roll();
+		d.roll();
+		d.roll();
+		d.roll();
+		
+		/*
+		 * after rolling 6 times, the 7th roll should start from pos 0 again
+		 */
 		d.roll();
 		assertEquals(d.getLastRoll(),1);
 		d.roll();
 		assertEquals(d.getLastRoll(),2);
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-	}
 	
-	@Test
-	public void test_roll_four_times_two_values()
-	{
-		SimDie d = new SimDie(new int[] {1,2});
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-		d.roll();
-		assertEquals(d.getLastRoll(),2);
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-		d.roll();
-		assertEquals(d.getLastRoll(),2);
-	}
-	
-	@Test
-	public void test_roll_five_times_two_values()
-	{
-		SimDie d = new SimDie(new int[] {1,2});
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-		d.roll();
-		assertEquals(d.getLastRoll(),2);
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
-		d.roll();
-		assertEquals(d.getLastRoll(),2);
-		d.roll();
-		assertEquals(d.getLastRoll(),1);
 	}
 
 }

3948c6283d738b75929ab9f567d30f13adae27b4
hoveronica <hoveronica01@gmail.com>
2018-02-16 11:36:43 -0600

    changed test_roll in LoadedDice class

--- Skunk/test/LoadedDiceTest.java
+++ Skunk/test/LoadedDiceTest.java
@@ -8,11 +8,15 @@ public class LoadedDiceTest
 	@Test
 	public void test_roll()
 	{
-		SimDie d1 = new SimDie(new int[]{1,2,3});
-		SimDie d2 = new SimDie(new int[] {4,5,6});
+		SimDie d1 = new SimDie(new int[]{1,2,3,4,5,6});
+		SimDie d2 = new SimDie(new int[] {6,5,4,3,2,1});
 		LoadedDice gameDice = new LoadedDice(d1,d2);
 		gameDice.dice_roll();
 		assertEquals(gameDice.dice_getLastRoll_die1(),1);
+		assertEquals(gameDice.dice_getLastRoll_die2(),6);
+		gameDice.dice_roll();
+		gameDice.dice_roll();
+		assertEquals(gameDice.dice_getLastRoll_die1(),3);
 		assertEquals(gameDice.dice_getLastRoll_die2(),4);
 	}
 

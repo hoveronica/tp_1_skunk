d7a6fc9a6edfcc6d33ff1d2dcc3792d67c0d022b
hoveronica <hoveronica01@gmail.com>
2018-03-01 14:52:09 -0600

    updated scoreUpdate method

--- Skunk/src/SkunkPlayer.java
+++ Skunk/src/SkunkPlayer.java
@@ -1,26 +1,30 @@
 //in game class, track players
 
-
-
-
 public class SkunkPlayer
 {
 	private String player_name;
-	private int score;
 	private int playerTotalScore;
 	private Turn gameTurn;
 	
 	public SkunkPlayer(String name) {
 		this.player_name = name;
+		gameTurn = new Turn();
+		playerTotalScore = 0;
+	}
+	
+	public String getName() {
+		return player_name;
 	}
 
-	public int playerScore() {  //keeps track of players total score 
+	//update score after turn
+	public int scoreUpdate() {
+		playerTotalScore = playerTotalScore + gameTurn.getTurnScore();
 		return playerTotalScore;
 	}
-	//come from end of roll
-	public int scoreUpdate() {
-		score = playerTotalScore + gameTurn.getTurnScore();
-		return score;
+	
+	//getter for playerScore
+	public int getScore() { 
+		return playerTotalScore;
 	}
 }
 
